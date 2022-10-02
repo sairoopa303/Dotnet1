1e087a28569b261bf161d97ad2bb84410cc209ce
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 16:24:01 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-class program5
+public class Program1
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        double fahrenheit;
-        double celsius = 6;
-        Console.WriteLine("Celsius: " + celsius);
-        fahrenheit = (celsius * 9) / 5 + 32;
-        Console.WriteLine("Fahrenheit: " + fahrenheit);
+        int int1, int2;
+        Console.Write("\n\n");
+        Console.Write("Check whether two integers are eual or not:\n");
+        Console.Write("----------------------------------------");
+        Console.Write("\n\n");
+        Console.Write("Inout 1st number: ");
+        int1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Inout 2nd number: ");
+        int2 = Convert.ToInt32(Console.ReadLine());
+        if (int1 == int2)
+            Console.WriteLine("{0} and {1} are equal.\n", int1, int2);
+        else
+            Console.Write("{0} and {1} are not equal.\n", int1, int2);
         Console.ReadLine();
     }
 }
